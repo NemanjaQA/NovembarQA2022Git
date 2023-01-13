74e79c362ea0930f9220406db10f3434f49c9b0f
Jelena Gligoric <jelenagligoric@gmail.com>
2023-01-13 20:40:15 +0100

    BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,9 +1,12 @@
 package pages;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -18,4 +21,23 @@ public class BasePage {
     public void waitVisibility(By by){
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(by));
     }
+
+    public void clickElement(By elementBy){
+        waitVisibility(elementBy);
+        driver.findElement(elementBy).click();
+    }
+    public void readText(By elementBy){
+            waitVisibility(elementBy);
+            driver.findElement(elementBy).getText();
+    }
+    public void writeText(By elementBy, String text){
+            waitVisibility(elementBy);
+            driver.findElement(elementBy).clear();
+            driver.findElement(elementBy).sendKeys();
+    }
+    public void assertStringEquals(String actualText, String expectedText){
+        Assert.assertEquals(actualText,expectedText);
+
+    }
 }
+
